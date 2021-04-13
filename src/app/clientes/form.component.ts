e2173991c049d16af04dfac2c7574a633d48a33c
jmedecerio <jmdecerio@gmail.com>
2021-04-13 12:36:46 +0200

    Incorporar sweetalert2

--- src/app/clientes/form.component.ts
+++ src/app/clientes/form.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Cliente } from './cliente';
 import { ClienteService } from './cliente.service';
 import { Router, NavigationExtras, ActivatedRoute } from '@angular/router';
+import swal from 'sweetalert2';
 
 @Component({
   selector: 'app-form',
@@ -21,7 +22,10 @@ export class FormComponent implements OnInit {
 
   public create(): void {
     this.clienteService.create(this.cliente).subscribe(
-      response => this.router.navigate(['../'], { relativeTo: this.route })
+      cliente => {
+        this.router.navigate(['../'], { relativeTo: this.route });
+        swal.fire("Cliente creado", `Cliente ${cliente.nombre} creado con exito`, 'success');
+      }
     );
   }
 }
